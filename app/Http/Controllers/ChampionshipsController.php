fa135ce60f1c572ff343986bf773d6ce340a66dc
Gabriel Morais <bielmorais434@gmail.com>
2026-03-28 17:07:48 -0300

    feat(championship): implement base structure (model, migration, controller, service)

---
+++ app/Http/Controllers/ChampionshipsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Championships;
+use Illuminate\Http\Request;
+
+class ChampionshipsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Championships $championships)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Championships $championships)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Championships $championships)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Championships $championships)
+    {
+        //
+    }
+}
