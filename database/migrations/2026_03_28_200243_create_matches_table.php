75dfdc557cad07bf26bea8a6aad4d7b0f257d94a
Gabriel Morais <bielmorais434@gmail.com>
2026-03-29 00:11:48 -0300

    fix(matches): add forgotten columns

--- database/migrations/2026_03_28_200243_create_matches_table.php
+++ database/migrations/2026_03_28_200243_create_matches_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
             $table->id();
             $table->foreignId('championship_id')->constrained('championships')->onDelete('cascade');
             $table->enum('phase', ['quarter', 'semi', 'third_place', 'final'])->default('quarter');
+            $table->integer('order');
+            $table->foreignId('team_home_id')->nullable()->constrained('teams');
+            $table->foreignId('team_away_id')->nullable()->constrained('teams');
+            $table->integer('goals_home')->nullable();
+            $table->integer('goals_away')->nullable();
+            $table->foreignId('winner_id')->nullable()->constrained('teams');
             $table->timestamps();
         });
     }
